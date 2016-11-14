9ce2a57e5ce44ad756cc1c9b0eed7768fab81f32
DeathByKorea <lewiszvvz@gmail.com>
2016-11-14 12:36:47 +0000

    5Spaces
    -Added code to remove whitespace and add spaces every 5

--- EncryptionDecryption/EncryptionDecryption/Module1.vb
+++ EncryptionDecryption/EncryptionDecryption/Module1.vb
@@ -56,6 +56,20 @@ Module Module1
         Console.WriteLine(unencrypt) 'prints to console  
         My.Computer.FileSystem.WriteAllText(save, unencrypt, False) ' saves file with given name
     End Sub
+    Sub removewhitespace()
+        Dim whitespace As String = My.Computer.FileSystem.ReadAllText(path & ".txt")
+        Dim final As String = 0
+        final = whitespace.Replace(" ", "")
+        Dim i As Integer = 5
+        While i < final.Length
+            final = final.Insert(i, " ")
+            i += 6
+        End While
+
+
+        Console.WriteLine(final)
+        My.Computer.FileSystem.WriteAllText(save & ".txt", final, False)
+    End Sub
     Dim choice As String
     Dim path As String
     Dim save As String
@@ -69,10 +83,11 @@ Module Module1
         Console.WriteLine("")
         Console.WriteLine("1: Encrypt sample.txt")
         Console.WriteLine("2: Decrypt sample.txt") ' menu
-        Console.WriteLine("3: Quit Program")
+        Console.WriteLine("3: Add spaces every 5 letters, WARNING, THIS WILL BREAK ORIGINAL SPACING")
+        Console.WriteLine("4: Quit Program")
         choice = Console.ReadLine()
         If IsNumeric(choice) Then
-            If choice = 1 Or choice = 2 Or choice = 3 Then ' Menu
+            If choice = 1 Or choice = 2 Or choice = 3 Or choice = 4 Then ' Menu
             Else
                 Console.ForegroundColor = ConsoleColor.Red
                 Console.WriteLine("Incorrect value") ' a bunch of if statements to mostly do the same thing up ahead
@@ -145,20 +160,28 @@ Module Module1
                 save = Console.ReadLine() 'get save location
                 save = save & ".txt"
                 decrypt()
-            ElseIf choice = 3 Then
-                Console.WriteLine("Are you sure you want to exit?")
-                choice = Console.ReadLine()
-                If choice = 1 Then
-                    main()
-                ElseIf choice = 2 Then 'exiting the program
-                    Environment.Exit(0.0)
-                Else
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("Incorrect Value")
-                    Threading.Thread.Sleep(1000)
-                    Console.ForegroundColor = ConsoleColor.White
-                End If
             End If
-        End If
+        ElseIf choice = 3 Then
+            Console.WriteLine("This will remove all whitespace and add a space every 5 letters, However. This will make the decrypted file harder to read as it will break original spacing.")
+            Console.WriteLine("Enter the name of the Encrypted file saved in %project%\bin\debug\.")
+            path = Console.ReadLine()
+            Console.WriteLine("Please enter the name of the file to be encrypted")
+            save = Console.ReadLine()
+            removewhitespace()
+
+        ElseIf choice = 4 Then
+            Console.WriteLine("Are you sure you want to exit?")
+            choice = Console.ReadLine()
+            If choice = 1 Then
+                main()
+            ElseIf choice = 2 Then 'exiting the program
+                Environment.Exit(0.0)
+            Else
+                Console.ForegroundColor = ConsoleColor.Red
+                Console.WriteLine("Incorrect Value")
+                Threading.Thread.Sleep(1000)
+                Console.ForegroundColor = ConsoleColor.White
+            End If
+            End If
     End Sub
 End Module
